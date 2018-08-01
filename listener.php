0a8972a7212d069c29afded6d31d25199706208b
Kevin Friedman <kevinfriedman@nypl.org>
2018-08-01 13:13:33 -0400

    Update error handling and Kinesis event

--- listener.php
+++ listener.php
@@ -4,6 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use NYPL\Starter\Config;
 use NYPL\Services\MailListener;
 use NYPL\Starter\APILogger;
+use NYPL\Starter\Listener\ListenerResult;
 
 try {
     Config::initialize(__DIR__ . '/config');
@@ -11,6 +12,15 @@ try {
     $mailListener = new MailListener();
 
     $mailListener->process($mailListener->getKinesisEvents());
-} catch (Exception $exception) {
-    APILogger::addError('Unable to process stream: ' . $exception->getMessage());
+} catch (\Throwable $exception) {
+    $message = 'Uncaught exception: ' . $exception->getMessage();
+
+    APILogger::addError($message, $exception);
+
+    echo json_encode(
+        new ListenerResult(
+            false,
+            $message
+        )
+    );
 }
